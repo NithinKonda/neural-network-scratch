da9fa9fb168fcf5aed794838708bacf2c8dd2060
NithinKonda <nithinkonda142@gmail.com>
2024-10-22 00:51:59 +0530

    new: Add Network

---
+++ src/lib/network.rs
@@ -0,0 +1,14 @@
+
+
+use super::matrix::Matrix;
+
+pub struct Network {
+    layers : Vec<usize>,
+    weights : Vec<Matrix>,
+    biases : Vec<Matrix>,
+    data : Vec<Matrix>,
+}
+
+impl Network {
+
+}
