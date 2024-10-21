1db6866cd1672babbb9a16b5fe1b32d466068cca
NithinKonda <nithinkonda142@gmail.com>
2024-10-21 23:54:51 +0530

    feat: Add matric add

--- src/matrix/matrix.rs
+++ src/matrix/matrix.rs
@@ -1,4 +1,4 @@
-use rand;
+
 
 pub struct Matrix{
     pub rows : usize,
@@ -7,6 +7,24 @@ pub struct Matrix{
 }
 
 impl Matrix {
+
+    pub fn add(&self, other : &Matrix) -> Matrix {
+        if self.rows != other.rows || self.cols != self.cols {
+            panic!("Wrong matric dimensions operation");
+        }
+
+        let mut buffer : Vec<f64> = Vec::<f64>::with_capacity(self.rows * self.cols);
+        for i in  0..self.data.len() {
+            let result : f64 = self.datap[i] + other.datap[i];
+            buffer.push(result);
+        }
+
+        Matrix {
+            rows: self.rows,
+            cols: self.cols,
+            data:buffer
+        }
+    }
 
 
     pub fn random(rows:usize, cols:usize) -> Matrix {
@@ -19,4 +37,7 @@ impl Matrix {
 
         Matrix{rows,cols,data:buffer}
     }
+
+
+
 }
