c51a9ac56c0384e992a4e71fefbd14a19ac6deb8
NithinKonda <nithinkonda142@gmail.com>
2024-10-23 01:25:43 +0530

    feat: Add Network new

--- src/lib/network.rs
+++ src/lib/network.rs
@@ -11,4 +11,21 @@ pub struct Network {
 
 impl Network {
 
+    pub fn new(layers: Vec<usize>) -> Network {
+        let mut weights = vec![];
+        let mut biases = vec![];
+        for i in 0..len(layers)-1 {
+            weights.push(Matrix::random(layers[i+1], layers[i]));
+            biases.push(Matrix::random(layers[i+1], 1));
+        }
+
+
+        Network{
+            layers,
+            weights,
+            biases,
+            data: vec![],
+        }
+    }
+
 }
