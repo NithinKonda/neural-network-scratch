6e467cc601fd0170a90605efb5a46e7342a704d3
NithinKonda <nithinkonda142@gmail.com>
2024-10-23 01:28:17 +0530

    fix: Change length of layers

--- src/lib/network.rs
+++ src/lib/network.rs
@@ -14,7 +14,7 @@ impl Network {
     pub fn new(layers: Vec<usize>) -> Network {
         let mut weights = vec![];
         let mut biases = vec![];
-        for i in 0..len(layers)-1 {
+        for i in 0..layers.len()-1 {
             weights.push(Matrix::random(layers[i+1], layers[i]));
             biases.push(Matrix::random(layers[i+1], 1));
         }
