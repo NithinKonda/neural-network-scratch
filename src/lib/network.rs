79e617e35cf64e7df915152afd47407aa467fee5
NithinKonda <nithinkonda142@gmail.com>
2024-10-23 01:42:43 +0530

    Add activation

--- src/lib/network.rs
+++ src/lib/network.rs
@@ -1,15 +1,16 @@
 
 
-use super::matrix::Matrix;
+use super::{activation::Activation, matrix::Matrix};
 
-pub struct Network {
+pub struct Network<'a> {
     layers : Vec<usize>,
     weights : Vec<Matrix>,
     biases : Vec<Matrix>,
     data : Vec<Matrix>,
+    activation : Activation<'a>,
 }
 
-impl Network {
+impl Network <'_>{
 
     pub fn new(layers: Vec<usize>) -> Network {
         let mut weights = vec![];
@@ -28,4 +29,19 @@ impl Network {
         }
     }
 
+
+    pub fn feed_forward(&mut self, inputs: Vec<f64>) -> Vec<f64> {
+        if inputs.len() != self.layers[0] {
+            panic!("Invalid number of inputs");
+        }
+        let mut current = Matrix::from(vec![inputs]).transpose();
+        self.data = vec![current.clone()];
+        for i in 0..self.layers.len()-1 {
+            current = self.weights[i].dot_multiply(&current).add(&self.biases[i]);
+            current = current.sigmoid();
+            self.data.push(current.clone());
+        }
+
+        current.data[0].clone()
+    }
 }
